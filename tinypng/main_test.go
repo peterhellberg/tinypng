d7a1bd55edbc2956f66912aee6eb6b806101f20c
Peter Hellberg <peter@c7.se>
2014-01-19 20:17:40 +0100

    Initial specs for the command

--- tinypng/main_test.go
+++ tinypng/main_test.go
@@ -8,12 +8,35 @@ import (
 )
 
 func TestTinypngCommand(t *testing.T) {
-	Convey("./tinypng", t, func() {
+	Convey("tinypng", t, func() {
+		Convey("outputs usage instructions if no args", func() {
+			out, _ := execGo("run", "main.go")
+
+			So(out, ShouldContainSubstring, "tinypng <input.png> [output.png]")
+		})
+
+		Convey("outputs error if unknown file", func() {
+			out, _ := execGo("run", "main.go", "unknown.png")
+
+			So(out, ShouldContainSubstring, "Input file does not exist.")
+		})
+
+		Convey("outputs error it invalid file", func() {
+			out, _ := execGo("run", "main.go", "../testdata/invalid.png")
+
+			So(out, ShouldContainSubstring, "Input file is not a valid PNG file.")
+		})
+
+		Convey("outputs note about adding TINYPNG_API_KEY to ENV", func() {
+			out, _ := execGo("run", "main.go", "../testdata/valid.png")
+
+			So(out, ShouldContainSubstring, "TINYPNG_API_KEY")
+		})
 	})
 }
 
-func runTinypng(inputFile, outputFile string) (string, error) {
-	out, err := exec.Command("go", "run", "main.go", inputFile, outputFile).CombinedOutput()
+func execGo(args ...string) (string, error) {
+	out, err := exec.Command("go", args...).CombinedOutput()
 
 	return string(out), err
 }
